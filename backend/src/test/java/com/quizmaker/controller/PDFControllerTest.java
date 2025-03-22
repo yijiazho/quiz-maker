7f4b395ce1ad420198625e3f2d1ea30fe13db435
yijiazho <tsihpemuh@gmail.com>
2025-03-21 23:28:59 -0700

    Fix the unit tests

--- backend/src/test/java/com/quizmaker/controller/PDFControllerTest.java
+++ backend/src/test/java/com/quizmaker/controller/PDFControllerTest.java
@@ -8,7 +8,8 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
@@ -18,7 +19,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.quizmaker.model.PDFContent;
 import com.quizmaker.service.PDFIngestionService;
 
-@WebMvcTest(PDFController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class PDFControllerTest {
 
     @Autowired
